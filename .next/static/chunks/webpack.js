356f7bb35aa5569cee0aae5a620cec5be0396f64
Michael <michaeldeanlindsay@gmail.com>
2023-12-07 13:04:33 -0800

    add home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8d39500c3eddae64"; }
+/******/ 		__webpack_require__.h = function() { return "7333fb2e04c5013a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
