6269f7427fe22ebfb7511f57eeb9d2b6a2bfab0d
Michael <michaeldeanlindsay@gmail.com>
2024-02-29 10:13:01 -0800

    text edits

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -50,7 +50,7 @@ export default function Projects() {
   return (
     <SimpleLayout
       title="Things I made."
-      intro="I've found that Kaggle, Reddit, etc. is where passion goes to die in a Jupyter notebook, so I'm trying to carve out my own little space to share what I enjoy doing."
+      intro="I've found that Kaggle, Reddit, etc. is where fun goes to die in a Jupyter notebook, so I'm trying to carve out a little space to share dumb projects I thought were funny."
     >
       <ul
         role="list"
